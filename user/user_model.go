27f233cc44eec890ce0a7954880eb7ecb4d69d54
gearcat <someone@example.com>
2024-02-28 11:01:51 +0000

    Fix addresses type and add more assets

--- user/user_model.go
+++ user/user_model.go
@@ -1,15 +1,41 @@
 package user
 
 type WalletCoin struct {
-	Addresses          []string `json:"addresses"`
-	AvailableBalance string   `json:"available_balance"`
+	Addresses			Addresses `json:"addresses"`
+	AvailableBalance	string `json:"available_balance"`
+	TotalBalance		string `json:"total_balance"`
+}
+
+type Addresses struct {
+	Address		[]string `json:"address"`
+	Tag			[]string `json:"tag"`
+	Network		[]string `json:"network"`
 }
 
 type Wallets struct {
 	Btc  WalletCoin `json:"btc"`
 	Btg  WalletCoin `json:"btg"`
+	Btt  WalletCoin `json:"btt"`
+	Busd WalletCoin `json:"busd"`
+	Cake WalletCoin `json:"cake"`
+	Celr WalletCoin `json:"celr"`
+	Chz  WalletCoin `json:"chz"`
+	Cocos WalletCoin `json:"cocos"`
+	Cos  WalletCoin `json:"cos"`
 	Cpy  WalletCoin `json:"cpy"`
+	Ctxc WalletCoin `json:"ctxc"`
+	Cvc  WalletCoin `json:"cvc"`
+	Dai  WalletCoin `json:"dai"`
+	Dash WalletCoin `json:"dash"`
+	Doge WalletCoin `json:"doge"`
+	Dot  WalletCoin `json:"dot"`
+	Dusk WalletCoin `json:"dusk"`
+	Dydx WalletCoin `json:"dydx"`
 	Elec WalletCoin `json:"elec"`
+	Enj  WalletCoin `json:"enj"`
+	Eos  WalletCoin `json:"eos"`
+	Erd  WalletCoin `json:"erd"`
+	Etc  WalletCoin `json:"etc"`
 	Eth  WalletCoin `json:"eth"`
 	Etp  WalletCoin `json:"etp"`
 	Jfin WalletCoin `json:"jfin"`
